b903e1b5c8151b5fc4d7b5029478753074a1beae
Daniel Xu <daniel.xu4@ontario.ca>
2024-01-12 14:46:53 -0500

    code clean up

--- mfe-shared/projects/mfe-shared-lib/src/lib/mfe-shared-lib.module.ts
+++ mfe-shared/projects/mfe-shared-lib/src/lib/mfe-shared-lib.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MfeSharedLibComponent } from './mfe-shared-lib.component';
-
-
 
 @NgModule({
   declarations: [
-    MfeSharedLibComponent
   ],
   imports: [
   ],
   exports: [
-    MfeSharedLibComponent
   ]
 })
 export class MfeSharedLibModule { }
--- mfe-shared/projects/mfe-shared-lib/src/public-api.ts
+++ mfe-shared/projects/mfe-shared-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of mfe-shared-lib
  */
 
-export * from './lib/mfe-shared-lib.service';
-export * from './lib/mfe-shared-lib.component';
 export * from './lib/mfe-shared-lib.module';
