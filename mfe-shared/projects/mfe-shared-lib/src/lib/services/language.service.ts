a48eaedc4d97dfb1388cf3da8953778d8f7856cd
Daniel Xu <daniel.xu4@ontario.ca>
2024-01-15 13:42:43 -0500

    Cache the language to keep it for page refresh

--- mfe-shared/projects/mfe-shared-lib/src/lib/services/language.service.ts
+++ mfe-shared/projects/mfe-shared-lib/src/lib/services/language.service.ts
@@ -5,10 +5,23 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class LanguageService {
-  private currentLang$ = new BehaviorSubject<string>('en');
+
+  DEFAULT_LANGUAGE: string = 'en';
+  LANGUAGE_CACHE: string = 'mfe-language';
+
+  private currentLang$: BehaviorSubject<string>;
+
+  constructor() {
+    // Check if there is a language stored in localStorage
+    const storedLang = localStorage.getItem(this.LANGUAGE_CACHE);
+    
+    // Initialize the BehaviorSubject with the stored language or default to 'en'
+    this.currentLang$ = new BehaviorSubject<string>(storedLang || this.DEFAULT_LANGUAGE);
+  }
 
   setLanguage(lang: string) {
     this.currentLang$.next(lang);
+    localStorage.setItem(this.LANGUAGE_CACHE, lang);
   }
 
   getLanguage() {
